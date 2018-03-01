b57f22c6a7c5f8bdb38bbd78e2d48a17b365fc94
afwbkbc <afwbkbc@inbox.lv>
2018-03-01 22:11:19 +0200

    aligner fixes

--- public/js/aligner.js
+++ public/js/aligner.js
@@ -7,20 +7,30 @@ $( document ).ready( function() {
 			var article = section.find( 'article' );
 			var h1 = section.find( '> h1 ');
 			if ( article.length > 0 && h1.length > 0 ) {
-				var sectionh = section.innerHeight();
+				
+				article.css( 'position', 'absolute' );
+				
+				var sectionh = $( window ).height() - headerh;
 				var sectionw = section.innerWidth();
 				var articleh = article.outerHeight();
 				var articlew = article.outerWidth();
 				var h1h = h1.outerHeight();
-				article.css( {
-					left: ( sectionw - articlew ) / 2 + 'px',
-					top: ( sectionh - articleh - headerh ) / 2 + h1h + 'px',
-				} );
+				
+				article.css( 'left', ( sectionw - articlew ) / 2 + 'px' );
+				
+				if ( sectionh - h1h > articleh ) {
+					article.css( 'top', ( sectionh - articleh - headerh ) / 2 + h1h + 'px' );
+					section.css( 'height', 'inherit' );
+				}
+				else if ( sectionh - h1h < articleh ) {
+					article.css( 'position', 'static' );
+					section.css( 'height', ( articleh + h1h ) + 'px' );
+				}
 			}
 		});
 	}
 	
 	$( window ).on( 'resize', align );
-	setTimeout( align, 1 );
+	setTimeout( align, 100 );
 	
 });
