d6fcecbed8b67cf7353b4d5738a7a39742215ad6
afwbkbc <afwbkbc@inbox.lv>
2018-03-01 21:34:53 +0200

    added fb reviews, fixed services

--- public/js/aligner.js
+++ public/js/aligner.js
@@ -5,14 +5,16 @@ $( document ).ready( function() {
 		$( 'section' ).each( function() {
 			var section = $(this);
 			var article = section.find( 'article' );
-			if ( article.length > 0 ) {
+			var h1 = section.find( '> h1 ');
+			if ( article.length > 0 && h1.length > 0 ) {
 				var sectionh = section.innerHeight();
 				var sectionw = section.innerWidth();
 				var articleh = article.outerHeight();
 				var articlew = article.outerWidth();
+				var h1h = h1.outerHeight();
 				article.css( {
 					left: ( sectionw - articlew ) / 2 + 'px',
-					top: ( sectionh - articleh ) / 2 - headerh + 'px',
+					top: ( sectionh - articleh - headerh ) / 2 + h1h + 'px',
 				} );
 			}
 		});
