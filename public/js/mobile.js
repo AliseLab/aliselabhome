ef8dd29ea8cb291b74a65fe9e32cdc8e058a2a31
afwbkbc <afwbkbc@inbox.lv>
2018-03-01 23:00:41 +0200

    mobile menu

---
+++ public/js/mobile.js
@@ -0,0 +1,24 @@
+$( document ).ready( function() {
+	
+	$( 'header .mobile-menu' ).on( 'click', function() {
+		var btn = $(this);
+		var nav = $( 'header nav' );
+		if ( btn.hasClass( 'active' ) ) {
+			btn.removeClass( 'active' );
+			nav.removeClass( 'mobile-visible' );
+		}
+		else {
+			btn.addClass( 'active' );
+			nav.addClass( 'mobile-visible' );
+		}
+	});
+	
+	$( 'header a' ).on( 'click', function() {
+		var btn = $( 'header .mobile-menu' );
+		if ( btn.hasClass( 'active' ) ) {
+			btn.removeClass( 'active' );
+			$( 'header nav' ).removeClass( 'mobile-visible' );
+		}
+	});
+	
+});
