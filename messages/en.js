8c6ab70c5a8a5a61cfb337e9ea08512c5c22219d
afwbkbc <afwbkbc@inbox.lv>
2018-03-04 17:39:10 +0200

    fixed en translations

--- messages/en.js
+++ messages/en.js
@@ -1,9 +1,9 @@
 exports.label = 'En';
 exports.trans = {
 
-	section_about: 'About us',
-	section_forwhom: 'For whom?',
-	section_forwhom_title: 'For whom',
+	section_about: 'About',
+	section_forwhom: 'For whom',
+	section_forwhom_title: 'For whom?',
 	section_services: 'Services and prices',
 	section_portfolio: 'Portfolio',
 	section_reviews: 'Reviews',
@@ -11,85 +11,102 @@ exports.trans = {
 	section_contacts: 'Contacts',
 	
 	// HOME
-	home_web_development: 'Web Development',
-	home_wanna_website: 'Need a website?',
-	home_order_now: 'Order Now!',
+	home_web_development: 'Web development',
+	home_wanna_website: 'Need a web-site?',
+	home_order_now: 'Order now!',
 	
 	// ABOUT US
-	about_us:
-		'<p>\
-			Мы - небольшая команда специалистов-фрилансеров в областивеб-разработки. Мы консультируем по вопросам развработки\
-			веб-сайтов, советуем наиболее подходящие решения. Разрабатываем landing page, адаптивные многостраничные сайты и\
-			интернет-магазины, основаясь на автоматизации бизнес-процессов. Нам очень важно, чтобы каждый созданный нами сайт\
-			имел высокое качество, так как сайт не только представляет клиента, но так же является нашей рекламой.\
-			<li>Мы предосталяем бесплатные консультации.</li>\
-			<li>Отсутствие выплат налогов, как в случае со штатным сотрудником, а следовально цены на услуги выгоднее.</li>\
-			<li>Нам не важна география, Вы можете находиться где угодно, чтобы отбратиться к нам за помощью.</li>\
-			<li>Договорные отношения с четко прописанными обязанностями и ответственностью Сторон.</li>\
-			<li>Высокая скорость исполнения задач</li>\
-			<li>Онлайн-поддержка 24/7</li>\
-			<li>В разработке каждого нового проекта и в поддержке существующих мы всегда думаем о том, какую пользу получит ваш клиент от взаимодействия с сайтом, от оптимизации процессов,от персонифицированных коммуникаций!</li>\
-		</p>',
+	about_us: '\
+		<p>\
+			We are a small team of freelancers in web development area. We provide web development consultations,\
+			suggesting most appropriate solutions. We develop landing pages, adaptive multipage websites and\
+			web stores, основаясь на автоматизации бизнес-процессов. We do our best to ensure that every our website\
+			gets high quality, because not only it represents our customer, but also is our advertisement.\
+		</p>\
+		<h3><div class="far fa-gem"></div>Advantages of working with us:</h3>\
+		<img src="/img/quality.png" class="quality"/>\
+		<ul>\
+			<li>We are providing free consultations.</li>\
+			<li>We are young company, so our prices are lower.</li>\
+			<li>We are not tied to geography, you can reside in any place in the world and use our services.</li>\
+			<li>Formal relations with documented expectations and responsibilities of Parties.</li>\
+			<li>High execution speed</li>\
+			<li>24/7 online support</li>\
+			<li>During development of every project and during support we always consider benefits that your client can get from interaction with your website, from process optimizations, from personalized communications!</li>\
+		</ul>\
+		',
 		
 	// FOR WHOM
-	for_whom_title: 'Кому подойдет наше предложение?',
+	for_whom_title: 'Who can get benefits of our services?',
 	for_whom_text: '\
-		<li>Для тех, кто хочет выйти на новый уровень прибыльности бизнеса.</li>\
-		<li>Сформировать клиентску базу.</li>\
-		<li>Получить целый арсенал для мотивации клиентов.</li>\
-		<li>Тем, кто хочет автоматизировать рутинные задачи.</li>\
-		<li>Тем, кто ценит своё время. </li>\
-		<li>Тем, кто хочет добиться большей эффективности отдела продаж. </li>\
+		<li>For those who wants to enter a new level of profits in his business.</li>\
+		<li>To form client base.</li>\
+		<li>To get arsenal to motivate clients.</li>\
+		<li>For those who want to automate routines.</li>\
+		<li>For those who cares about his time.</li>\
+		<li>For those who want to increase efficiency of marketing.</li>\
 	',
-		
+	
 	// SERVICES
 	services_1_title: 'Landing page',
 	services_1_text: '\
-		<li>Lorem ipsum dolor sit amet,</li>\
-		<li>consectetur adipiscing elit.</li>\
-		<li>Vivamus at augue imperdiet</li>\
+		<li>Написание продающего текста</li>\
+		<li>Разработка статического прототипа</li>\
+		<li>Разработка продающего макета</li>\
 	',
-	services_2_title: 'Home page',
+	services_2_title: 'Corporate website',
 	services_2_text: '\
-		<li>Lorem ipsum dolor sit amet,</li>\
-		<li>consectetur adipiscing elit.</li>\
-		<li>Vivamus at augue imperdiet</li>\
+		<li>Написание продающего текста</li>\
+		<li>Разработка статического прототипа</li>\
+		<li>Разработка продающего макета</li>\
 	',
-	services_3_title: 'Webstore',
+	services_3_title: 'E-commerce',
 	services_3_text: '\
-		<li>Lorem ipsum dolor sit amet,</li>\
-		<li>consectetur adipiscing elit.</li>\
-		<li>Vivamus at augue imperdiet</li>\
+		<li>Написание продающего текста</li>\
+		<li>Разработка статического прототипа</li>\
+		<li>Разработка продающего макета</li>\
 	',
-	services_order_now: 'Order Now',
-		
+	services_order_now: 'Order now',
+	
 	// QUESTIONS
 	questions: '\
 		<div>\
-			<h2>вопрос блабла ?</h2>\
-			<p>Lorem ipsum dolor sit amet,consectetur adipiscing elit. Vivamus at augue imperdietor sit amet,consectetur adipiscing elit. Vivamus at augue imperdietor siterdiet</p>\
+			<h2>What is the purpose of website?</h2>\
+			<p>Website is important marketing tool in internet. It can be used to keep in touch with existing clients, informing them of important events and services of company. It can be used to attract new clients. It can also be used to support image and respectability of business. It can be used to aid trade, to optimize business processes and reduce required human resources. Finally, it can be used for entertainment purposes.</p>\
 		</div>\
 		<div>\
-			<h2>вопрос блабла ?</h2>\
-			<p>Lorem ipsum dolor sit amet,consectetur adipiscing elit. Vivamus at augue imperdietor sit amet,consectetur adipiscing elit. Vivamus at augue imperdietor siterdiet</p>\
+			<h2>What is the process of website creation?</h2>\
+			<p>Our clients do not need to have technical expertise. All you need to do is contact us, by website, email or phone. Our manager will get in touch and together you will create technical requirements and determine project difficulty. 50% of payment is to be paid upfront.</p>\
 		</div>\
 		<div>\
-			<h2>вопрос блабла ?</h2>\
-			<p>Lorem ipsum dolor sit amet,consectetur adipiscing elit. Vivamus at augue imperdietor sit amet,consectetur adipiscing elit. Vivamus at augue imperdietor siterdiet</p>\
+			<h2>How long are you in website development business?</h2>\
+			<p>We are in this area since 2014.</p>\
+		</div>\
+		<div>\
+			<h2>How long does it take to create a website?</h2>\
+			<p>It depends on website complexity, amount of content and desired functionality and efficiency of third-party specialists, if any. Typically one project requires from 2 weeks to 3 months of development.</p>\
+		</div>\
+		<div>\
+			<h2>What are your strong sides?</h2>\
+			<p>We are using latest web technologies. We provide high execution speed. We can also help with marketing.</p>\
+		</div>\
+		<div>\
+			<h2>Is there support after completiong of project?</h2>\
+			<p>Yes. You will be eligible for 3 months of free support.</p>\
 		</div>\
 	',
 
 	// CONTACTS
-	contacts_name: 'Имя',
-	contacts_phone: 'Телефон',
+	contacts_name: 'Name',
+	contacts_phone: 'Phone',
 	contacts_email: 'E-Mail',
-	contacts_landing: 'Визитка',
-	contacts_corporate: 'Корпоративный',
-	contacts_ecommerce: 'Интернет магазин',
-	contacts_comments: 'Дополнительная информация',
-	contacts_send: 'Отправить',
-	contacts_mailresult_success: 'Спасибо, форма отправлена, свяжемся в ближайшее время!',
-	contacts_mailresult_error: 'Ошибка отправки формы, напишите нам сами на <a href="mailto:${email}">${email}</a>',
+	contacts_landing: 'Landing page',
+	contacts_corporate: 'Corporate website',
+	contacts_ecommerce: 'E-commerce',
+	contacts_comments: 'Additional information',
+	contacts_send: 'Send',
+	contacts_mailresult_success: 'Thank you, request was submitted and we will get in touch!',
+	contacts_mailresult_error: 'There was an error sending your request, please e-mail us manually at <a href="mailto:${email}">${email}</a>',
 	
 	// footer
 	all_rights_reserved: 'All rights reserved © Aliselab 2018',
