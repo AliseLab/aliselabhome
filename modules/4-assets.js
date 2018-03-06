27722fe14cf1534bf86f4e77f7ceb387b85bc604
afwbkbc <afwbkbc@inbox.lv>
2018-03-06 22:53:36 +0200

    added assets manager

--- modules/4-assets.js
+++ modules/4-assets.js
@@ -4,11 +4,45 @@ exports.run = function( data, next ) {
 	if ( data.config.debug )
 		data.twig.cache( false );
 	
-	data.fs.readdir( './public/js', (err, js) => {
-		data.fs.readdir( './public/css', (err, css) => {
-			data.js = js;
-			data.css = css;
-			data.app.use( data.express.static( __dirname + '/../public' ) );
+	data.app.use( data.express.static( __dirname + '/../public' ) );
+	
+	data.fs.readdir( './assets/js', (err, js) => {
+		data.fs.readdir( './assets/css', (err, css) => {
+			
+			if ( data.config.debug ) {
+				data.app.use( data.express.static( __dirname + '/../assets' ) );
+				data.js = js;
+				data.css = css;
+			}
+			else {
+				var assets = {
+					'app.js' : {
+						type: 'js',
+						dir: 'js',
+						main: 'app.js',
+						files: [ '*.js' ],
+					},
+					'style.css' : {
+						type: 'css',
+						dir: 'css',
+						main: 'style.css',
+						files: [ '*.css' ],
+					},
+				};
+				
+				var config = {
+					rootRoute : '/',
+					srcDir : __dirname + '/../assets',
+					buildDir : './public',
+					process : 'false',
+					env: 'production'
+				};
+				
+				data.app.use( require( 'express-asset-manager' )( assets, config ) );
+				data.js = [ 'app.js' ];
+				data.css = [ 'style.css' ];
+			}
+			
 			next();
 		});
 	});
