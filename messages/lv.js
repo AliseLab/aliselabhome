c40507dc7159303724a50cbd63500bca48ca41cf
afwbkbc <afwbkbc@inbox.lv>
2018-03-14 20:46:47 +0000

    added lv

---
+++ messages/lv.js
@@ -0,0 +1,105 @@
+exports.label = 'Lv';
+exports.trans = {
+
+	section_about: 'Par mums',
+	section_forwhom: 'Kam',
+	section_forwhom_title: 'Kam ir piemērots mūsu piedāvājums?',
+	section_services: 'Piedāvājumi un cents',
+	section_portfolio: 'Portfolio',
+	section_reviews: 'Atsauksmes',
+	section_questions: 'Jautājumi',
+	section_contacts: 'Kontakti',
+	
+	// HOME
+	home_web_development: 'Mājas lapas izstrāde',
+	home_wanna_website: 'Jums ir nepieciešama mājas lapa?',
+	home_order_now: 'Pasūtīt!',
+	
+	// ABOUT US
+	about_us: '\
+		<p>\
+			 Mēs esam neliela speciālistu komanda, kas nodarbojas ar mājas lapu izstrādi. Mēs konsultējam jautājumos, kas ir saistīti ar mājas lapu izstrādi,\
+           iesakām piemērotākos risinājumus. Izstrādājam mājas lapas (vizītkartes), adaptīvas vairāku lapaspušu mājas lapas un interneta veikalus.\
+            Mums ir ārkārtīgi svarīgi, katrai izveidotajai mājas lapai nodrošināt augstu\
+            jo mājas lapa nav tikai mūsu klienta tēls, bet arī mūsu reklāma.\
+		</p>\
+		<h3><span class="far fa-gem"></span>Priekšrocības strādājot ar mums:</h3>\
+		<img src="/img/quality.png" class="quality"/>\
+		<ul>\
+			<li>Mēs piedāvājam bezmaksas konsultācijas.</li>\
+			<li>Mēs esam jauns uzņēmums, tāpēc piedāvājam izdevīgākas pakalpojumu cenas.</li>\
+			<li>Mums nav būtiska ģeogrāfija, Jūs varat atrasties jebkurā vietā un izmantot mūsu pakalpojumus.</li>\
+			<li>Līgumiskas attiecības ar stingri reglamentētiem Pušu pienākumiem un atbildībām.</li>\
+			<li>Augsts uzdevumu izpildīšanas ātrums</li>\
+			<li> Tiešsaistes atbalsts 24/7</li>\
+			<li>Izstrādājot katru jaunu projektu un atbalstot iepriekš izveidotos mēs vienmēr domājam par to, kādu labumu iegūs Jūsu klients no mājas lapas izmantošanas, no procesu optimizēšanas, no profesionālās komunikācijas! </li>\
+		</ul>\
+		',
+		
+	// FOR WHOM
+	for_whom_title: 'Кam ir piemērots mūsu piedāvājums?',
+	for_whom_text: '\
+		<li>Tiem, kas grib sasniegt jaunus peļņas gūšanas augstumus. </li>\
+		<li>Izveidot klientu bāzi.</li>\
+		<li>Iegūt veselu arsenālu ar klientu motivēšanas rīkiem.</li>\
+		<li>Tiem, kas vēlas automatizēt rutīnas uzdevumus.</li>\
+		<li>Tiem, kas novērtē savu laiku</li>\
+		<li>Tiem, kas grib paaugstināt pārdošanas nodaļas efektivitāti.</li>\
+	',
+	
+	// SERVICES
+	services_1_title: 'Vizītkartes mājas lapa',
+	services_1_text: 'Vizītkartes mājas lapa, kas ir īpaši paredzēts pakalpojumu vai preču pārdošanai',
+	services_2_title: 'Korporatīvā mājas lapa',
+	services_2_text: 'Korporatīvā mājas lapa var saturēt foto galeriju, pasūtījumu veikšanas formu, blogu un nelielu katalogu',
+	services_3_title: 'Interneta veikals',
+	services_3_text: 'Interneta veikals parasti satur pirkumu grozu, preču meklēšanas filtrus un jaunt ziņu lapu',
+	services_order_now: 'Pasūtīt',
+	services_from: 'no',
+	services_day: 'dienas',
+	services_week: 'nedēļas',
+	services_weeks: 'nedēļām',
+	
+	// QUESTIONS
+	questions: '\
+		<div>\
+			<h2>Ar kādu nolūku veido mājas lapu?</h2>\
+			<p>Mājas lapa ir būtisks interneta mārketinga instruments, ko izmanto lai uzturētu attiecības ar esošiem klientiem, informēt tos par aktuāliem notikumiem un pakalpojumiem, ko nodrošina uzņēmums. Otrs svarīgs mērķis ir jaunu klientu piesaiste. Trešais populārākais mērķis ir uzņēmuma tēla un prestižas uzturēšana. Ceturtais mērķis ir pārdošana. Piektais, bet tika pat svarīgais, mērķis ir biznesa procesu optimizācija ar mērķi samazināt cilvēka resursu izmantošanu procesu veikšanā. Sestais mērķis ir izklaide. Septītais populārākais mājas lapu mērķis ir biznesa projekts.</p>\
+		</div>\
+		<div>\
+			<h2>Kāds ir mājas lapas izveidošanas process?</h2>\
+			<p> Klientam nav jāiedziļinās mājas lapas izstrādāšanas tehnoloģijā un izvietošanā. Jūs veicat pasūtījumu vai piezvanāt mums. Mūsu menedžeris sazinās ar jums un jūs kopā sastādāt tehnisko uzdevumu. Saskaņojat darba veikšanas termiņus. Veicat priekšapmaksu 50% apmērā.</p>\
+		</div>\
+		<div>\
+			<h2>Cik ilgi Jūs nodarbojaties ar mājas lapu izstrādāšanu?</h2>\
+			<p>Ar mājas lapu izstrādāšanu šajā nozarē mēs nodarbojamies no 2014. gada.</p>\
+		</div>\
+		<div>\
+			<h2>Cik ilgu laiku aizņem mājas lapas izveidošana?</h2>\
+			<p>Mājas lapas izveidošanas laiks ir atkarīgs no projekta funkcionālā kontenta apjoma un iesaistīto pasūtītāja speciālistu aktivitātes, kas ir atbildīgi par detaļu saskaņošanu. Vidēji viss process aizņem no 2 nedēļām līdz 3 mēnešiem.  </p>\
+		</div>\
+		<div>\
+			<h2>Kādas ir Jūsu stiprās puses?</h2>\
+			<p>Mēs izmantojam jaunās dizaina tehnoloģijas. Veidojam mājas lapas salīdzinoši īsos termiņos. Palīdzam ar mārketingu. </p>\
+		</div>\
+		<div>\
+			<h2>Vai būs pieejams atbalsts pēc mājas lapas izstrādes?</h2>\
+			<p> Jā. Bezmaksas atbalsts būs pieejams 3 mēnešus. </p>\
+		</div>\
+	',
+
+	// CONTACTS
+	contacts_name: 'Vārds',
+	contacts_phone: 'Telefons',
+	contacts_email: 'E-pasts',
+	contacts_landing: 'Vizītkarte',
+	contacts_corporate: 'Korporatīvais',
+	contacts_ecommerce: 'Interneta veikal',
+	contacts_comments: 'Papildus informācija',
+	contacts_send: 'Nosūtīt',
+	contacts_mailresult_success: 'Paldies, pieteikums ir nosūtīts. Mēs sazināsimies ar Jums tuvākajā laikā!',
+	contacts_mailresult_error: 'Kļūda pieteikuma nosūtīšanā, uzrakstiet mums uz e-pastu <a href="mailto:${email}">${email}</a>',
+	
+	// footer
+	all_rights_reserved: 'Visas tiesības aizsargātas © Aliselab 2018',
+};
